1cebc40be95824f02e2b648c2d2b288a694ec42e
soheilamir <soheil.amir67@gmail.com>
2018-09-03 14:16:42 +0430

    last update with amir in 97.6.12

--- src/HaftAsemanAbi/HaftAsemanView/SBS/SBSOtherModules/ViewWeb/MenuService/HeaderMenuCtrl.js
+++ src/HaftAsemanAbi/HaftAsemanView/SBS/SBSOtherModules/ViewWeb/MenuService/HeaderMenuCtrl.js
@@ -1,5 +1,12 @@
 ﻿
 'use strict'
 angular.module('headerMenuModule').controller('headerMenuCtrl', ['$scope', '$rootScope', function ($scope, $rootScope) {
-
+    $scope.ShowSection_1 = false;
+    $scope.ShowSection_2 = false;
+    this.HideSection = function () {
+        $scope.ShowSection_1 = false;
+    };
+    this.HideSection_2 = function () {
+        $scope.ShowSection_2 = false;
+    };
 }]);
