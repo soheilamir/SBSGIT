13824d2e9b58254a87ecfe14dba254871916dd54
soheilamir <soheil.amir67@gmail.com>
2018-10-06 11:59:10 +0330

    amir commit project. 14.7.97

---
+++ src/HaftAsemanAbi/HaftAsemanView/SBS/SBSOtherModules/ViewWeb/MenuService/InnerMenuDirective.js
@@ -0,0 +1,13 @@
+﻿
+'use strict'
+angular.module('headerMenuModule').directive('innerMenu', [function () {
+    return {
+        replace: true,
+        require: '^baseMenu',
+        scope: {
+        },
+        templateUrl: "/HaftAsemanView/SBS/SBSOtherModules/ViewWeb/MenuService/InnerMenuTmpl.html",
+        link: function (scope, ele, attr, ctrl) {
+        }
+    };
+}]);
