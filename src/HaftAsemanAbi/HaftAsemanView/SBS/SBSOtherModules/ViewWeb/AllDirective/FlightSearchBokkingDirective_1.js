7977fd8bd6938121d1a91ce8da8f4b9616b23a0f
AmirSoheil <software@rrfco.com>
2018-10-08 12:16:13 +0330

    amir

---
+++ src/HaftAsemanAbi/HaftAsemanView/SBS/SBSOtherModules/ViewWeb/AllDirective/FlightSearchBokkingDirective_1.js
@@ -0,0 +1,12 @@
+﻿
+'use strict'
+angular.module('AllDirectiveModule').directive('flightSearchBooking', [function () {
+    return {
+        replace: true,
+        scope: {
+        },
+        templateUrl: "/HaftAsemanView/SBS/SBSOtherModules/ViewWeb/AllDirective/flightSearchBookingTmpl.html",
+        link: function (scope, ele, attr) {
+        }
+    };
+}]);
