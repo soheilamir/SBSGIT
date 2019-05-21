bd8dbcbf5fedccbc5a9dac612104ebf4100d8b37
soheilamir <amir.sohiel@gmail.com>
2019-05-21 11:51:27 +0430

    commiting

---
+++ src/HaftAsemanAbi/HaftAsemanView/SBS/SBSOtherModules/ViewWeb/Register_Login_Service/LogRegSecDirective.js
@@ -0,0 +1,19 @@
+﻿
+'use strict'
+angular.module('Login_Reg_ServiceModule').directive('loginRegBase', [function () {
+    return {
+        replace: true,
+        require: '^?loginRegService',
+        templateUrl: "/HaftAsemanView/SBS/SBSOtherModules/ViewWeb/Register_Login_Service/LogRegBaseTmpl.html",
+        link: function (scope, element, attrs,ctrl) {
+            var swiperlogreg = new Swiper('.swiper-logreg-container', {
+                autoHeight: true, //enable auto height
+                navigation: {
+                    nextEl: '.swiper-button-next',
+                    prevEl: '.swiper-button-prev',
+                },
+                speed: 2000,
+            });
+        },
+    };
+}]);
